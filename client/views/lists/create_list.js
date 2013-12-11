d04acc9b4eb229141f86811e018cd7ed89cb1fc6
Steve Chen <sebboh@gmail.com>
2013-12-11 11:22:02 -0800

    fixed bug for redirecting to list after creating it

--- client/views/lists/create_list.js
+++ client/views/lists/create_list.js
@@ -10,7 +10,7 @@ Template.createList.events({
       if (error)
         return alert(error.reason);
 
-      Meteor.Router.to('listItems', list); 
+      Meteor.Router.to('listItems', id); 
     });
   }
 });
