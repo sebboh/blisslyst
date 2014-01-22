005f957b1a20ea5768d399394f5b47ec3a99bb4f
Steve Chen <sebboh@gmail.com>
2014-01-21 23:00:59 -0800

    sorts lists in reverse order of creation date

--- client/views/lists/all_lists.js
+++ client/views/lists/all_lists.js
@@ -1,6 +1,6 @@
 Template.allLists.helpers({
   lists: function() {
     // only show lists that are not archived
-    return Lists.find({ $or: [ { archive: {$exists: false } }, { archive: false } ] });
+    return Lists.find({ $or: [ { archive: {$exists: false } }, { archive: false } ] }, {sort: { submitted: -1 }});
   }
 });
--- client/views/lists/archived_lists.js
+++ client/views/lists/archived_lists.js
@@ -1,6 +1,6 @@
 Template.archivedLists.helpers({
   lists: function() {
     // only show lists that are not archived
-    return Lists.find({ archive: true });
+    return Lists.find({ archive: true }, {sort: { submitted: -1 }});
   }
 });
