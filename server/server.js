7c6ad4afcc6577a0d7000f8664c86d484c82a0dc
Steve Chen <sebboh@gmail.com>
2014-04-27 00:21:59 -0700

    adds comment for calling setPassword method, adds header.js file that was missing

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ if (Meteor.isServer) {
 
 Meteor.methods({
   setPassword: function(userId, password) {
+    // run this in the console: Meteor.call("setPassword", userId, password);
     if (this.userId == 'tSNEkH3Ft8F5rje3S') Accounts.setPassword(userId, password);
   }    
 });
